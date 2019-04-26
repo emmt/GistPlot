0eda9f57e03172a68fece32ce8b9db1b820c8828
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2019-04-26 16:55:00 +0200

    Clarify code

--- play/any/psoftfpe.c
+++ play/any/psoftfpe.c
@@ -1,7 +1,11 @@
-/* psoftfpe.c
- * raise pending software SIGFPE using fenv.h
- */
-/* Copyright (c) 2015, David H. Munro.
+/*
+ * psoftfpe.c -
+ *
+ * Raise pending software SIGFPE using fenv.h
+ *
+ *------------------------------------------------------------------------------
+ *
+ * Copyright (c) 2015, David H. Munro.
  * All rights reserved.
  * This file is part of yorick (http://yorick.sourceforge.net).
  * Read the accompanying LICENSE file for details.
@@ -17,34 +21,42 @@
 #include <signal.h>
 
 /* FE_INEXACT and FE_UNDERFLOW are not really exception conditions */
-#define PL_TRUE_EXCEPTIONS FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW
+#define TRUE_EXCEPTIONS (FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW)
 
 /* code should use PL_SOFTFPE_TEST macro, never call this directly */
 void
 pl_softfpe(void)
 {
-  if (fetestexcept(PL_TRUE_EXCEPTIONS)) {
-    feclearexcept(PL_TRUE_EXCEPTIONS);
+  if (fetestexcept(TRUE_EXCEPTIONS)) {
+    feclearexcept(TRUE_EXCEPTIONS);
     raise(SIGFPE);
   }
 }
 
-static fenv_t pl_fenv;
-static int pl_valid_fenv = 0;
-static int pl_depth_fenv = 0;  /* zero means at pl_fenv */
-
 void
 pl_fpehandling(int on)
 {
-  pl_valid_fenv = (pl_valid_fenv || !fegetenv(&pl_fenv));
-  if (pl_valid_fenv) {
-    if (on) {
-      if (on != 1) pl_depth_fenv = 1;
-      if (pl_depth_fenv && !--pl_depth_fenv)
-        fesetenv(&pl_fenv);
-    } else {
-      if (!pl_depth_fenv++)
+  static fenv_t fenv;
+  static int initialized = 0;
+  static int depth = 0;  /* zero means at memorized fenv */
+
+  if (initialized == 0 && fegetenv(&fenv) == 0) {
+    initialized = 1;
+  }
+  if (initialized) {
+    if (on == 0) {
+      if (depth == 0) {
         fesetenv(FE_DFL_ENV);
+      }
+      ++depth;
+    } else {
+      if (on != 1) {
+        depth = 1;
+      }
+      if (depth == 1) {
+        fesetenv(&fenv);
+      }
+      --depth;
     }
   }
 }
